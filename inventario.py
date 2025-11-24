9e8bf52e72328b72d931f3ccf443cba6ed4d5a42
asistenteconta-blip <asistenteconta@casaaguizotes.com>
2025-11-24 11:57:34 -0600

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -17,7 +17,7 @@ scope = [
 DOC_NAME = "Copia de MACHOTE INV BATANGA DDMMAAAA  ACT25"
 
 # --- CREDENCIALES DESDE secrets.toml ---
-service_info = st.secrets["gcp_service_account"]
+service_info = st.secrets["google_service_account"]
 
 credentials = Credentials.from_service_account_info(
     service_info,
@@ -446,3 +446,4 @@ if st.session_state.get("confirm_reset", False):
         if st.button("❌ Cancelar"):
             st.info("Operación cancelada, no se modificó nada.")
             st.session_state["confirm_reset"] = False
+
